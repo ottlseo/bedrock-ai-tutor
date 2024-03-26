96dd4d8119997b52cace296d5b4a04315b4a5b93
yoonseo <ottlseo@amazon.com>
2024-03-26 11:27:48 +0000

    Add js code

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import StartTranscriptionButton from './components/StartTranscriptionButton';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <StartTranscriptionButton />
+    </div>
+  );
+}
+
+export default App;
