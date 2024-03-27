9bb20f9825f10864e19133179c9524518782af83
Yoonseo Kim <ottlseo@amazon.com>
2024-03-27 20:57:38 +0900

    Update api url to Localhost for testing

--- frontend/src/libs/bedrockClient.js
+++ frontend/src/libs/bedrockClient.js
@@ -1,9 +1,9 @@
 
 import { REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } from "./cred.js";
 import axios from 'axios';
-import { API_URL } from "./cred.js";
+// import { API_URL } from "./cred.js";
 
-const apiEndpoint = API_URL+"/test";
+const apiEndpoint = "http://127.0.0.1:8080/test" // API_URL+"/test";
 export const callApi = async (prompt) => {
     try {
         const request = {
