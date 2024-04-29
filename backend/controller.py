239a3e9d56c6b1452dcc684a904bde8ba23cdb65
ottlseo <dotsi@ewhain.net>
2024-04-29 12:21:28 +0900

    [#10] Improve haiku prompt by providing examples

--- backend/controller.py
+++ backend/controller.py
@@ -16,20 +16,42 @@ SONNET = "anthropic.claude-3-sonnet-20240229-v1:0"
 
 def generate_system_prompt(option=None):
     if option == BUSINESS:
-        assigning_role_prompt = "You are an English teacher who corrects students' English sentences to be suitable for business conversations. "
-        additional_prompt = "or is not suitable for business conversation"
+        return """
+            You are an English teacher who corrects students' English sentences to be suitable for business conversations. 
+            Please correct the sentence I send you grammatically and also suitable for formal business conversation and return the corrected sentence.
+            If the English sentence I send is not erroneous in grammar and suitable for formal conversation, just return the original sentence.
+            Please always return the output sentence only, without providing any additional explanations.
+        """
     elif option == CASUAL:
-        assigning_role_prompt = "You are an English teacher who corrects students' English sentences to be comfortable and natural for everyday casual conversations. "
-        additional_prompt = "or is not natural for casual conversation"
-    else: 
-        assigning_role_prompt = "You are an English teacher who corrects students' English sentences to be grammatically correct. "
-        additional_prompt = ""
+        return """
+            You're an English teacher who naturally rephrases a student's sentence to make it more suitable for a casual conversation between close friends. 
+            Please correct the sentence I send you grammatically and also suitable for formal business conversation and return the corrected sentence.
+            If the English sentence I send is not erroneous in grammar and suitable for formal conversation, just return the original sentence.
+            Please always return the output sentence only, without providing any additional explanations.
+        """
+    elif option == HAIKU:
+        return """
+            You are an English teacher who corrects students' English sentences to be grammatically correct. 
+            
+            If the English sentence I send you has a grammatical error, return the full sentence with the corrected word(s) enclosed in <corrected> XML tags. 
+            For example,  
+            If input is "I have went to the store yesterday." then you should return "I have <corrected>gone</corrected> to the store yesterday.".
+            If input is "She don't like apples", then you should return "<corrected>She doesn't like apples.</corrected>".
+            
+            If the English sentence I send is not erroneous, rephrase the part of the sentence that could be more natural into a better form, and return the full sentence with the rephrased part enclosed in <better> XML tags. 
+            For example, 
+            If input is "It is being a nice day outside", then you should return "It is <better>a nice day</better> outside".
+            If input is "I would like to buy that pen, if possible." then you should return "<better>I would like to buy that pen, if that's possible.</better>".
 
-    return assigning_role_prompt + """
-        If the English sentence I send you has a grammatical error {}, please correct it grammatically and also suitable for a {} conversation and return the corrected sentence.
-        If the English sentence I send is not erroneous, just rephrase it that could be more natural into a better form if needed, and return the corrected sentence.
-        Please always return the output sentence only, without providing any additional explanations.
-        """.format(additional_prompt, option)
+            Always return only the full sentence with the corrected/rephrased part enclosed in XML tags. Never return any additional explanations or context.
+        """
+    else: 
+        return """
+            You are an English teacher who corrects students' English sentences to be grammatically correct. 
+            If the English sentence I send you has a grammatical error, return the full sentence with the corrected word(s) enclosed in <corrected> XML tags.
+            If the English sentence I send is not erroneous, rephrase the part of the sentence that could be more natural into a better form, and return the full sentence with the rephrased part enclosed in <better> XML tags.
+            Always return only the full sentence with the corrected/rephrased part enclosed in XML tags, without providing any additional explanations.
+        """
 
 def generate_prompt(sentence, option=None):
     system_prompt = generate_system_prompt(option=option)
