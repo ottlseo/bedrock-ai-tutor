db398313e0ae95b99068aa5d4d1a5fe02c0100e2
ottlseo <dotsi@ewhain.net>
2024-05-02 03:16:51 +0900

    Update sample react app

--- anycompany/src/App.js
+++ anycompany/src/App.js
@@ -8,6 +8,7 @@ import sampleImage2 from './assets/sample2.jpeg';
 import sampleImage3 from './assets/sample3.jpeg';
 import sampleImage4 from './assets/sample4.png';
 import sampleImage5 from './assets/sample5.jpeg';
+import sampleImage6 from './assets/sample6.jpeg';
 
 const App = () => {
   const videoRef = useRef();
@@ -25,6 +26,7 @@ const App = () => {
         <div className='sample-video-section'>
           <h2>수강생 후기 Video sample</h2>
           <video
+            controls width="400px"
             muted // 무음
             autoPlay // 자동 재생
             loop // 반복 재생
@@ -36,33 +38,43 @@ const App = () => {
         </div>
         <div className='sample-image-section'>
           <h2>Main Image Sample</h2>
-          <img src={sampleImage5} width='550px'/><br/>
+          <img src={sampleImage5} width='400px'/><br/>
+        </div>
+        <div className='sample-video-section'>
+          <h2>기업 소개 Video sample</h2>
+          <video
+            controls width="400px"
+            muted // 무음
+            autoPlay // 자동 재생
+            loop // 반복 재생
+            ref={videoRef}
+            onCanPlay={() => setPlayBackRate()}
+          >
+            <source src={sampleVideo2} type="video/mp4" />
+          </video>
         </div>
       </div>
 
       <div className='sample-images-section'>
         <img src={sampleImage4} width='100%'/>
       </div>
+      
+      <div id="sample-assets-container">
+        <div className='sample-images-section'>
+          <img src={sampleImage1}/><br/>
+        </div>
+        <div className='sample-images-section'>
+          <img src={sampleImage3}/>
+        </div>
+      </div>
+      
       <div className='sample-images-section'>
-        <img src={sampleImage1} width='50%'/><br/>
+        <img src={sampleImage6} width='100%'/><br/>
       </div>
       <div className='sample-images-section'>
-        <img src={sampleImage2} width='50%'/><br/>
+        <img src={sampleImage2} width='100%'/><br/>
       </div>
-      <div className='sample-images-section'>
-        <img src={sampleImage3} width='50%'/>
-      </div>
-      <div className='videoSection'>
-        <video
-          muted // 무음
-          autoPlay // 자동 재생
-          loop // 반복 재생
-          ref={videoRef}
-          onCanPlay={() => setPlayBackRate()}
-        >
-          <source src={sampleVideo2} type="video/mp4" />
-        </video>
-      </div>
+      
     </div>
   );
 }
