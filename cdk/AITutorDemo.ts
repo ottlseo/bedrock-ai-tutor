499f0fe6c5e08f283c9cce1d1bd7c2a1fed30895
ottlseo <dotsi@ewhain.net>
2024-05-08 13:10:42 +0900

    Update frontend cdk stack

--- cdk/AITutorDemo.ts
+++ cdk/AITutorDemo.ts
@@ -3,12 +3,4 @@ import * as cdk from "aws-cdk-lib";
 import { WebStack } from "./WebStack";
 
 const app = new cdk.App();
-new WebStack(app, "WebStack"
-    // {
-    // env: {
-    //     account: app.node.tryGetContext('aws:accountId'),
-    //     region: 'us-east-1',
-    //   },
-    // }
-        // "aws:accountId": "590183997414",
-);
+new WebStack(app, "WebStack");
