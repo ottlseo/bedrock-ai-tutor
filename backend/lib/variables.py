adefdc44b63b2483416dad490ea3ba488cc398d1
ottlseo <dotsi@ewhain.net>
2024-07-03 21:37:36 +0900

    Refactor: remove flask api (to be replaced by serverless)

--- backend/lib/variables.py
+++
@@ -1,8 +0,0 @@
-
-BUSINESS_CONVERSATION = 'business'
-CASUAL_CONVERSATION = 'casual'
-NORMAL_CONVERSATION_USING_SONNET = 'sonnet-normal'
-NORMAL_CONVERSATION_USING_HAIKU = 'haiku-normal'
-
-HAIKU = "anthropic.claude-3-haiku-20240307-v1:0" 
-SONNET = "anthropic.claude-3-sonnet-20240229-v1:0"
