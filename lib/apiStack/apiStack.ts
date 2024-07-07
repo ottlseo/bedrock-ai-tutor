a5d6ba01d4854c0764374547184b4b01fd471ef2
ottlseo <dotsi@ewhain.net>
2024-07-07 19:23:01 +0900

    [#18] Fix: api gateway internal server error by solving CORS header & integrationResponse settings

--- lib/apiStack/apiStack.ts
+++ lib/apiStack/apiStack.ts
@@ -22,7 +22,7 @@ export class ApiStack extends Stack {
     });
 
     // Lambda 함수 생성
-    const lambdaFunction1 = new lambda.Function(this, "callHaikuLambda", {
+    const haikuLambdaFunction = new lambda.Function(this, "callHaikuLambda", {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("./lib/apiStack/lambdas"),
       handler: "callHaikuLambda.lambda_handler",
@@ -32,46 +32,81 @@ export class ApiStack extends Stack {
       initialPolicy: [bedrockAccessPolicy], // Bedrock 액세스 권한 부여
     });
 
-    const lambdaFunction2 = new lambda.Function(this, "callSonnetLambda", {
+    const sonnetLambdaFunction = new lambda.Function(this, "callSonnetLambda", {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("./lib/apiStack/lambdas"),
       handler: "callSonnetLambda.lambda_handler",
       layers: [layer],
-      timeout: Duration.minutes(10), // 제한 시간 10분으로 설정
-      memorySize: 512, // 메모리 할당량 512MB로 설정
-      initialPolicy: [bedrockAccessPolicy], // Bedrock 액세스 권한 부여
+      timeout: Duration.minutes(10),
+      memorySize: 512,
+      initialPolicy: [bedrockAccessPolicy],
     });
 
-    const lambdaFunction3 = new lambda.Function(this, "generateBusinessVerLambda", {
+    const businessLambdaFunction = new lambda.Function(this, "generateBusinessVerLambda", {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("./lib/apiStack/lambdas"),
       handler: "generateBusinessVerLambda.lambda_handler",
       layers: [layer],
-      timeout: Duration.minutes(10), // 제한 시간 10분으로 설정
-      memorySize: 512, // 메모리 할당량 512MB로 설정
-      initialPolicy: [bedrockAccessPolicy], // Bedrock 액세스 권한 부여
+      timeout: Duration.minutes(10),
+      memorySize: 512,
+      initialPolicy: [bedrockAccessPolicy],
     });
 
     // API Gateway 생성
     const api = new apigw.RestApi(this, "AiTutorAPI", {
       restApiName: "AiTutorAPI",
       description: "Bedrock AI Tutor API created by CDK",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS,
+        allowHeaders: apigw.Cors.DEFAULT_HEADERS,
+      },
+      endpointTypes: [apigw.EndpointType.REGIONAL], // endpoint 유형을 Regional로 설정
     });
-    // TODO: API GW Policy 추가
 
-    // Lambda 함수를 API Gateway에 연결
-    const lambdaIntegration1 = new apigw.LambdaIntegration(lambdaFunction1);
-    const lambdaIntegration2 = new apigw.LambdaIntegration(lambdaFunction2);
-    const lambdaIntegration3 = new apigw.LambdaIntegration(lambdaFunction3);
+    // Lambda proxy 설정 - 통합 응답, 메서드 응답 정의
+    const integrationResponse = {
+      proxy: false,
+      integrationResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Origin": "'*'",
+          },
+          responseTemplates: {
+            "application/json": ""
+          },
+        },
+      ],
+    };
 
-    api.root.addResource("haiku").addMethod("GET", lambdaIntegration1);
-    api.root.addResource("sonnet").addMethod("GET", lambdaIntegration2);
-    api.root.addResource("business").addMethod("GET", lambdaIntegration3);
+    const methodResponse = {
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Origin": true, // Response headers를 Access-Control-Allow-Origin으로 설정
+          },
+          responseModels: {
+            "application/json": apigw.Model.EMPTY_MODEL, // Response body를 application/json으로 빈 값으로 설정
+          },
+        },
+      ],
+    };
+
+    // Lambda 함수를 API Gateway에 POST 메서드로 연결
+    const haikuLambdaIntegration = new apigw.LambdaIntegration(haikuLambdaFunction, integrationResponse);
+    const sonnetLambdaIntegration = new apigw.LambdaIntegration(sonnetLambdaFunction, integrationResponse);
+    const businessLambdaIntegration = new apigw.LambdaIntegration(businessLambdaFunction, integrationResponse);
+
+    api.root.addResource("haiku").addMethod("POST", haikuLambdaIntegration, methodResponse);
+    api.root.addResource("sonnet").addMethod("POST", sonnetLambdaIntegration, methodResponse);
+    api.root.addResource("business").addMethod("POST", businessLambdaIntegration, methodResponse);
 
     // API Gateway URL 출력
-    new CfnOutput(this, "GatewayUrl", {
-      value: "https://"+api.url,
-      description: "API Gateway endpoint URL for Prod stage",
+    new CfnOutput(this, "ApiGatewayUrl", {
+      value: `https://${api.url}`,
+      description: "AI Tutor API Gateway endpoint URL for Prod stage",
     });
   }
 }
