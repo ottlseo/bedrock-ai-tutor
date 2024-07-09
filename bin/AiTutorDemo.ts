9bd7e63d51f26b04ac988b4fb312dae1946e7b04
ottlseo <dotsi@ewhain.net>
2024-07-09 19:55:41 +0900

    Fix: syntax error

--- bin/AiTutorDemo.ts
+++ bin/AiTutorDemo.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { ApiStack } from "../lib/apiStack/apiStack"
-import { WebStack } from "../lib/webStack/webStack";;
+import { ApiStack } from "../lib/apiStack/apiStack";
+import { WebStack } from "../lib/webStack/webStack";
 
 const app = new cdk.App();
 
