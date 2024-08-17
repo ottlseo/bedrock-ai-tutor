b07e091b06029f0e7e30bbd8047f6c10896aad8c
ottlseo <dotsi@ewhain.net>
2024-08-18 01:33:42 +0900

    Feat: update casual ver lambda code & add frontend build file

--- lib/apiStack/lambdas/generateCasualVerLambda.py
+++ lib/apiStack/lambdas/generateCasualVerLambda.py
@@ -17,13 +17,13 @@ def generate_prompt(sentence):
     return "System: "+ system_prompt + "\n\nUser: "+ sentence +"\n\nAssistant: "
     
 
-def call_claude_v3(prompt, model=HAIKU):
+def call_claude_v3(prompt):
     body = json.dumps({
         "max_tokens": 2000,
         "messages": [{"role": "user", "content": generate_prompt(prompt)}],
         "anthropic_version": "bedrock-2023-05-31"
     })
-    modelId = model
+    modelId = SONNET35
     metadata = "application/json"
 
     try:
